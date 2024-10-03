b60248101c4983efc6d03ad7507c42f8ed424266
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-10-03 20:12:45 +0200

    feat: implement service locator for dependency management

---
+++ lib/core/service_locator.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:netflix_app/features/movies/data/data_sources/tmdb_api_service.dart';
+import 'package:netflix_app/features/movies/domain/usecases/get_popular_movies.dart';
+
+import '../features/movies/data/repositories/movie_repository_impl.dart';
+import '../features/movies/domain/repositories/movie_repository.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeServiceLocator() async {
+  // ! External
+  sl.registerLazySingleton<Dio>(() => Dio());
+  // ! Services
+  sl.registerLazySingleton<TMDBApiService>(() => TMDBApiServiceImpl(sl<Dio>()));
+  // ! Repositories
+  sl.registerLazySingleton<MovieRepository>(() => MovieRepositoryImpl());
+  // ! Use cases
+  sl.registerLazySingleton<GetPopularMovies>(() => GetPopularMovies());
+
+}
