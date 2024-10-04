2c5fa92fc45d4b43fce0e266a586c11143825d64
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-10-04 19:11:34 +0200

    feat: add route for movie details page

--- lib/core/configs/routes/app_router.dart
+++ lib/core/configs/routes/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_app/core/configs/routes/routes.dart';
+import 'package:netflix_app/features/movies/presentation/pages/movie_details_page.dart';
 
 import '../../../features/home/presentation/pages/home_page.dart';
 import '../../../features/splash/presentation/pages/splash_page.dart';
@@ -11,6 +12,9 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const SplashPage());
       case Routes.home:
         return MaterialPageRoute(builder: (_) => const HomePage());
+      case Routes.movieDetail:
+        final movieId = settings.arguments as int;
+        return MaterialPageRoute(builder: (_) => MovieDetailsPage(movieId: movieId));
       default:
         return null;
     }
