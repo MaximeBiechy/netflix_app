a152efec1335422a1d58f9bcd7a20e8a78e09651
Maxime Biechy <maxime.biechy5@etu.univ-lorraine.fr>
2024-10-04 19:11:06 +0200

    feat: add new themes to AppTheme

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:netflix_app/core/configs/theme/app_colors.dart';
 
@@ -7,6 +5,17 @@ class AppTheme {
   static final darkTheme = ThemeData(
     primaryColor: AppColors.background,
     brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.background,
     fontFamily: 'Roboto',
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+          fontSize: 28, fontWeight: FontWeight.bold, color: AppColors.white),
+      titleMedium: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.bold, color: AppColors.white),
+      bodyMedium: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.normal, color: AppColors.white),
+      labelSmall: TextStyle(
+          fontSize: 12, fontWeight: FontWeight.normal, color: Colors.grey),
+    ),
   );
 }
